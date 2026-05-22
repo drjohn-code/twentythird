4421c456201e06902fb4def276e939e58a4eb94e
drjohn-code <dr.johnm95@gmail.com>
2026-05-22 14:13:55 +0300

    icon debuging

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,9 +40,6 @@ export const metadata: Metadata = {
     siteName: "TwentyThird",
     type: "website",
   },
-icons: {
-    icon: "/icon.png",
-  },
 };
 
 const themeBootstrap = `(function(){try{var t=localStorage.getItem('theme');if(t)document.documentElement.setAttribute('data-theme',t);}catch(e){}})();`;
